fbac04190e26c5581612fd0e6cf1cf0f4fcfea4b
mutiara ramadhani <mutiara20ti@mahasiswa.pcr.ac.id>
2021-07-20 20:02:37 +0700

    Upload Project

---
+++ src/Klinik/Konfig.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Klinik;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Konfig {
+    private static Connection MySQLConfig;
+    public static Connection configDB() throws SQLException{
+        try{
+            String url = "jdbc:mysql://localhost:3306/klinik";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+            
+        } catch (SQLException e){
+            System.out.println("koneksi gagal "+e.getMessage());
+        }
+        return MySQLConfig;
+    }
+}
